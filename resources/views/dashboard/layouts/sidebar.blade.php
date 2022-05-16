7c901a33424ac49794ab7064816180d231d463b1
mehdi <mehdiprogrammer30@gmail.com>
2022-05-16 22:11:45 +0430

    Edit Panel

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -14,24 +14,28 @@
             <!-- Sidebar Menu -->
             <nav>
                 <ul class="sidebar-menu" data-widget="tree">
-                    <li class="active">
-                        <a href="master.blade.php">
-                            <i class="zmdi zmdi-view-dashboard"></i>
-                            <span>داشبورد</span>
-                        </a>
-                    </li>
-                    <li class="treeview">
-                        <a href="javascript:void(0)">
-                            <i class="zmdi zmdi-apps"></i>
-                            <span>مدیریت کاربران</span>
-                            <i class="fa fa-angle-left"></i>
-                        </a>
+                    <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="/dashboard"><i class="zmdi zmdi-view-dashboard"></i><span>داشبورد</span></a></li>
+{{--                    <li class="treeview @php if((request()->is('dashboard/products')) || (request()->is('dashboard/products/create'))) { echo 'active';} @endphp">--}}
+{{--                        <a href="javascript:void(0)"><i class="zmdi zmdi-account"></i> <span>مدیریت محصولات</span> <i class="fa fa-angle-left"></i></a>--}}
+{{--                        <ul class="treeview-menu">--}}
+{{--                            <li><a href="{{ route('products.index') }}" style="{{ request()->is('dashboard/products') ? 'color:blue' : '' }}">لیست محصولات</a></li>--}}
+{{--                            <li><a href="{{ route('products.create') }}" style="{{ request()->is('dashboard/products/create') ? 'color:blue' : '' }}">افزودن محصول</a></li>--}}
+{{--                        </ul>--}}
+{{--                    </li>--}}
+                    <li class="treeview @php if((request()->is('dashboard/users')) || (request()->is('dashboard/users/create'))) { echo 'active';} @endphp">
+                        <a href="javascript:void(0)"><i class="zmdi zmdi-account"></i> <span>مدیریت کاربران</span> <i class="fa fa-angle-left"></i></a>
                         <ul class="treeview-menu">
-                            <li>
-                                <a href="/users">همه کاربران</a>
-                            </li>
+                            <li><a href="{{ route('users.index') }}" style="{{ request()->is('dashboard/users') ? 'color:blue' : '' }}">لیست کاربران</a></li>
+                            <li><a href="{{ route('users.create') }}" style="{{ request()->is('dashboard/users/create') ? 'color:blue' : '' }}">افزودن کاربر</a></li>
                         </ul>
                     </li>
+{{--                    <li class="treeview @php if((request()->is('dashboard/comments')) || (request()->is('dashboard/comments/unapproved'))) { echo 'active';} @endphp">--}}
+{{--                        <a href="javascript:void(0)"><i class="zmdi zmdi-account"></i> <span>مدیریت نظرات</span> <i class="fa fa-angle-left"></i></a>--}}
+{{--                        <ul class="treeview-menu">--}}
+{{--                            <li><a href="{{ route('comments.index') }}" style="{{ request()->is('dashboard/comments') ? 'color:blue' : '' }}">لیست نظرات</a></li>--}}
+{{--                            <li><a href="{{ route('comments.getUnapproved') }}" style="{{ request()->is('dashboard/comments/unapproved') ? 'color:blue' : '' }}">نظرات تائید نشده</a></li>--}}
+{{--                        </ul>--}}
+{{--                    </li>--}}
                 </ul>
             </nav>
         </div>
